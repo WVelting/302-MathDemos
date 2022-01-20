738078cb8beeb9656e82bd8361dabc21556f0312
William Velting <wvelting@gmail.com>
2022-01-20 15:14:55 -0500

    Week 2 Lab 1
    +Added Ease Demo
    +Added AniMath

---
+++ UnityProject/Assets/Scripts/animMath.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class animMath 
+{
+
+    public static float Map(float v, float mina, float maxa, float minb, float maxb){
+        float p = (v-mina)/(maxa-mina);
+        return Lerp(minb, maxb, p);
+    }
+    public static float Lerp(float a, float b, float p)
+    {
+
+        return (b-a)*p+a;
+    }
+
+    public static Vector3 Lerp(Vector3 a, Vector3 b, float p)
+    {
+        Vector3 result = new Vector3();
+
+        result.x = Lerp(a.x, b.x, p);
+        result.y = Lerp(a.y, b.y, p);
+        result.z = Lerp(a.z, b.z, p);
+
+        return result;
+    }
+
+    public static Quaternion Lerp(Quaternion a, Quaternion b, float p){
+
+        Quaternion rot = Quaternion.identity;
+
+        rot.x = Lerp(a.x, b.x, p);
+        rot.y = Lerp(a.y, b.y, p);
+        rot.z = Lerp(a.z, b.z, p);
+        rot.w = Lerp(a.w, b.w, p);
+
+        return rot;
+    }
+
+    public static float Ease(float current, float target, float percentLeftAfter1Second, float dt) {
+    float p = 1 - Mathf.Pow(percentLeftAfter1Second, dt);
+    return Lerp(current, target, p);
+}
+
+public static Vector3 Ease(Vector3 current, Vector3 target, float percentLeftAfter1Second, float dt = -1){
+    if(dt<0) dt = Time.deltaTime;
+    float p = 1 - Mathf.Pow(percentLeftAfter1Second, dt);
+    return new Vector3(current.x, target.x, p);
+}
+}
